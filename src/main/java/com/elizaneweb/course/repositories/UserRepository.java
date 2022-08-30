cb6b80e76091009846a8b34c4ba62d4aaf323faa
elizane <elizanedutra3011@gmail.com>
2022-08-30 08:30:37 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/elizaneweb/course/repositories/UserRepository.java
+++ src/main/java/com/elizaneweb/course/repositories/UserRepository.java
@@ -1,2 +1,8 @@
-package com.elizaneweb.course.repositories;public class UserRepository {
+package com.elizaneweb.course.repositories;
+
+import com.elizaneweb.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
