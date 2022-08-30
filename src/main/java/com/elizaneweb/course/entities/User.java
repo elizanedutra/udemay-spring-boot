c02baabe7557a908cf50c7012641b7688d880f00
elizane <elizanedutra3011@gmail.com>
2022-08-30 14:59:53 -0300

    Order, Instant, ISO 8601 - parte 1

--- src/main/java/com/elizaneweb/course/entities/User.java
+++ src/main/java/com/elizaneweb/course/entities/User.java
@@ -2,6 +2,8 @@ package com.elizaneweb.course.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +18,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -65,6 +70,11 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
